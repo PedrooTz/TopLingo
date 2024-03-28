1e0a9d00a03efc0fe123c5d9901b535c44f27eb5
Pedro Henrique <pedrohenrique1603@gmail.com>
2024-03-28 11:31:46 -0300

    Em processo

--- main.js
+++ main.js
@@ -1,12 +1,80 @@
-function toggleDarkMode() {
-    var body = document.body;
-    var button = document.getElementById("button-modo");
+'use strict'
 
-    if (body.classList.contains("dark-mode")) {
-        body.classList.remove("dark-mode");
-        button.textContent = "Ativar Modo Claro"; 
+const textareaFrom = document.querySelector("#textareaFrom");
+const textareaTo = document.querySelector("#textareaTo");
+const selects = document.querySelectorAll("select");
+
+const countries = {
+    "en-GB": "Inglês",
+    "es-ES": "Espanhol",
+    "it-IT": "Italiano",
+    "ja-JP": "Japonês",
+    "pt-BR": "Português",
+};
+
+selects.forEach((tag) => {
+    for (let country in countries) {
+        let selected;
+        if (tag.className.includes("selectFrom") && country == "pt-BR") {
+            selected = "selected";
+        } else if (tag.className.includes("selectTo") && country == "en-GB") {
+            selected = "selected";
+        }
+
+        const option = `<option value="${country}" ${selected}>${countries[country]}</option>`;
+
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+textareaFrom.addEventListener("keypress", (event) => {
+    if (event.code === "Enter") {
+        loadTranslation();
+    }
+});
+
+function loadTranslation() {
+    fetch(
+            `https://api.mymemory.translated.net/get?q=${textareaFrom.value}&langpair=${selects[0].value}|${selects[1].value}`
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            textareaTo.value = data.responseData.translatedText;
+        });
+}
+
+// ******************************************************************************************
+
+botaoTema.addEventListener('keypress', (event) => {
+    if(event.code === 'Enter'){
+        mudarBackgroundAlice()
+    }
+})
+
+const botaoTema = document.getElementById('button-theme')
+const body = document.getElementById('body')
+
+function mudarTema() {
+    
+    if(botaoTema.children[0].src.includes('sol')){
+        botaoTema.children[0].src = './img/lua 1.png'
+        botaoTema.children[0].alt = 'Lua'
     } else {
-        body.classList.add("dark-mode");
-        button.textContent = "Ativar Modo Escuro"; 
+        botaoTema.children[0].src = './img/sol.png'
+        botaoTema.children[0].alt = 'Sol'
+    }
+    botaoTema.classList.toggle('tema-escuro')
+    body.classList.toggle('tema-escuro')
+}
+
+// ******************************************************************************************
+const mudarBackgroundAlice = function(){
+    if(botaoTema.value == "alice".toLowerCase()){
+        console.log(botaoTema.value)
+        mudarTema()
+    }else{
+        loadTranslation()
     }
 }
+
+botaoTema.addEventListener('click', mudarTema)
